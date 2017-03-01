9ab00721e57a441a5ac26a637621cefa15dc2a73
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-03-01 15:08:17 +0100

    jenkins: add value_string termination check
    Change-Id: I7fe3678b524d602fc6aa14bc0ed06308df809a3e

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+./contrib/verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
 autoreconf --install --force
 ./configure --enable-static --enable-sanitize
 $MAKE $PARALLEL_MAKE check \
