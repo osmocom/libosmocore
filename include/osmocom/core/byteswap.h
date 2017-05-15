3318c657dea52c143842dab0e0f7733288f7bd4d
Harald Welte <laforge@gnumonks.org>
2017-05-15 12:07:51 +0200

    introduce byteswap.h with osmo_{htonl,ntohl,htons,ntohs}
    We need to have an architecture-independend way of endian conversion /
    byte swapping functions which will also work on embedded (bare iron)
    builds.   Let's introduce osmocom/core/bytesawp.h for this purpose.
    
    Change-Id: Ibc0cc1e36d4ed63a35cf8ceff3af0f26e5ac7a3d

---
+++ include/osmocom/core/byteswap.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <stdint.h>
+#include <osmocom/core/endian.h>
+
+/*! \brief byte-swap a 32bit word
+ *  \param[in] in to be swapped 32bit word
+ *  \returns byte-swapped 32bit word */
+static inline uint32_t osmo_swab32(uint32_t in)
+{
+	uint32_t out;
+
+	out = (in & 0xff) << 24;
+	out |= (in & 0xff00) << 8;
+	out |= (in & 0xff0000) >> 8;
+	out |= (in & 0xff000000) >> 24;
+
+	return out;
+}
+
+/*! \brief byte-swap a 16bit word
+ *  \param[in] in to be swapped 16bit word
+ *  \returns byte-swapped 16bit word */
+static inline uint16_t osmo_swab16(uint16_t in)
+{
+	uint16_t out;
+
+	out = (in & 0xff) << 8;
+	out |= (in & 0xff00) >> 8;
+
+	return out;
+}
+
+#ifdef OSMO_IS_LITTLE_ENDIAN
+#define osmo_ntohl(x)	osmo_swab32(x)
+#define osmo_ntohs(x)	osmo_swab16(x)
+#define osmo_htonl(x)	osmo_swab32(x)
+#define osmo_htons(x)	osmo_swab16(x)
+#else
+#define osmo_ntohl(x)	(x)
+#define osmo_ntohs(x)	(x)
+#define osmo_htonl(x)	(x)
+#define osmo_htons(x)	(x)
+#endif
---
+++ tests/endian/endian_test.c
@@ -0,0 +1,31 @@
+#include <osmocom/core/byteswap.h>
+#include <osmocom/core/utils.h>
+
+int main(int argc, char **argv)
+{
+	printf("Testing 16bit swappinng\n");
+	OSMO_ASSERT(osmo_swab16(0x1234) == 0x3412);
+	printf("Testing 32bit swappinng\n");
+	OSMO_ASSERT(osmo_swab32(0x12345678) == 0x78563412);
+
+	printf("Testing ntohX() and htonX()\n");
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#if OSMO_IS_LITTLE_ENDIAN == 0
+#error "Something wrong with endianness detection!"
+#endif /* IS_LITTLE_ENDIAN */
+	OSMO_ASSERT(osmo_ntohs(0x1234) == 0x3412);
+	OSMO_ASSERT(osmo_htons(0x1234) == 0x3412);
+	OSMO_ASSERT(osmo_htonl(0x12345678) == 0x78563412);
+	OSMO_ASSERT(osmo_ntohl(0x12345678) == 0x78563412);
+#else
+#if OSMO_IS_LITTLE_ENDIAN == 1
+#error "Something wrong with endianness detection!"
+#endif /* IS_LITTLE_ENDIAN */
+	OSMO_ASSERT(osmo_ntohs(0x1234) == 0x1234);
+	OSMO_ASSERT(osmo_htons(0x1234) == 0x1234);
+	OSMO_ASSERT(osmo_htonl(0x12345678) == 0x12345678);
+	OSMO_ASSERT(osmo_ntohl(0x12345678) == 0x12345678);
+#endif /* __BYTE_ORDER__ */
+
+	exit(0);
+}
