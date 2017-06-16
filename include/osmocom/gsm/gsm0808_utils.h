4703fd215f3684a2e7cba46e423512960218ea54
Philipp Maier <pmaier@sysmocom.de>
2017-06-16 16:17:19 +0200

    cosmetic: remove API documentation strings in header file
    Typically we don't place comments with the function declarations
    in .h files. Not sure why this file has comments for each. The
    API doc belongs in the .c file as proper doxygen comments,
    and shouldn't be duplicated in the .h file.
    
    In this particular case, doxygen comments are added in the
    corresponding c file recently.
    
    Change-Id: I5c4cb55be5ec59a6945b1720b875c5987f1cfaad

--- include/osmocom/gsm/gsm0808_utils.h
+++ include/osmocom/gsm/gsm0808_utils.h
@@ -23,55 +23,31 @@ struct sockaddr_storage;
 
 #include <osmocom/gsm/protocol/gsm_08_08.h>
 
-/* Encode AoIP transport address element */
 uint8_t gsm0808_enc_aoip_trasp_addr(struct msgb *msg,
 				    const struct sockaddr_storage *ss);
-
-/* Decode AoIP transport address element */
 int gsm0808_dec_aoip_trasp_addr(struct sockaddr_storage *ss,
 				const uint8_t *elem, uint8_t len);
-
-/* Encode Speech Codec element */
 uint8_t gsm0808_enc_speech_codec(struct msgb *msg,
 				 const struct gsm0808_speech_codec *sc);
-
-/* Decode Speech Codec element */
 int gsm0808_dec_speech_codec(struct gsm0808_speech_codec *sc,
 			     const uint8_t *elem, uint8_t len);
-
-/* Encode Speech Codec list */
 uint8_t gsm0808_enc_speech_codec_list(struct msgb *msg,
 				      const struct gsm0808_speech_codec_list
 				      *scl);
-
-/* Decode Speech Codec list */
 int gsm0808_dec_speech_codec_list(struct gsm0808_speech_codec_list *scl,
 				  const uint8_t *elem, uint8_t len);
-
-/* Encode Channel Type element */
 uint8_t gsm0808_enc_channel_type(struct msgb *msg,
 				 const struct gsm0808_channel_type *ct);
-
-/* Decode Channel Type element */
 int gsm0808_dec_channel_type(struct gsm0808_channel_type *ct,
 			     const uint8_t *elem, uint8_t len);
-
-/* Encode Encryption Information element */
 uint8_t gsm0808_enc_encrypt_info(struct msgb *msg,
 				 const struct gsm0808_encrypt_info *ei);
-
-/* Decode Encryption Information element */
 int gsm0808_dec_encrypt_info(struct gsm0808_encrypt_info *ei,
 			     const uint8_t *elem, uint8_t len);
-
-/* Encode Cell Identifier List element */
 uint8_t gsm0808_enc_cell_id_list(struct msgb *msg,
 				 const struct gsm0808_cell_id_list *cil);
-
-/* Decode Cell Identifier List element */
 int gsm0808_dec_cell_id_list(struct gsm0808_cell_id_list *cil,
 			     const uint8_t *elem, uint8_t len);
-
 int gsm0808_chan_type_to_speech_code(uint8_t perm_spch);
 int gsm0808_speech_codec_from_chan_type(struct gsm0808_speech_codec *sc,
 					uint8_t perm_spch);
