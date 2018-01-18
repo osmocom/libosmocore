1ac277dcef694a2f849a6b5259cf5aa4488bebb9
Max <msuraev@sysmocom.de>
2018-01-18 19:15:40 +0100

    jenkins: add dispatcher script
    Similar to the way we test osmo-bts, add simple dispatcher script which
    calls appropriate test depending on a given parameter. This will allow
    to simplify the job description. While at it, also rename jenkins-arm.sh
    -> jenkins_arm.sh to match the OsmoBTS.
    
    The older scripts are preserved for compatibility and shall be removed
    once we update job description in osmo-ci.
    
    Change-Id: I2955e866bce4f000a53369bd601a346c36c82468

---
+++ contrib/jenkins_arch.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# this is a dispatcher script which will call the arch-specific
+# script based on the arch specified as command line argument
+
+arch="$1"
+
+if [ "x$arch" == "x" ]; then
+	echo "Error: You have to specify the architecture as first argument, e.g. $0 amd64"
+	exit 2
+fi
+
+if [ ! -d "./contrib" ]; then
+  echo "Run ./contrib/jenkins_arch.sh from the root of the libosmocore tree"
+  exit 1
+fi
+
+set -x -e
+
+case "$arch" in
+
+  amd64)
+    ./contrib/jenkins_amd64.sh
+  ;;
+
+  arch)
+    ./contrib/jenkins_arch.sh
+  ;;
+
+  *)
+    set +x
+    echo "Unexpected architecture '$arch'"
+  ;;
+esac
