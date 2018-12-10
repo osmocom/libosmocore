2bf01d439cbbf346c333043ec0c82e6d5ef62ee2
Pau Espin Pedrol <pespin@sysmocom.de>
2018-12-10 10:59:13 +0100

    stats.h: Fix build on MacOS
    Otherwise struct osmo_stats_config is not defined and compiler fails.
    
    Related: OS#3722
    Change-Id: I037c3ca141ecee2d457e0a881a56e32ee24cec4d

--- include/osmocom/core/stats.h
+++ include/osmocom/core/stats.h
@@ -28,7 +28,7 @@
  * embedded systems.  We cannot use the autoconf-defined HAVE_... macros
  * here, as that only works at library compile time, not at application
  * compile time */
-#ifdef unix
+#if defined(unix) || defined(__APPLE__)
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -141,5 +141,5 @@ int osmo_stats_reporter_send_buffer(struct osmo_stats_reporter *srep);
 int osmo_stats_reporter_udp_open(struct osmo_stats_reporter *srep);
 int osmo_stats_reporter_udp_close(struct osmo_stats_reporter *srep);
 
-#endif /* unix */
+#endif /* unix || __APPLE__ */
 /*! @} */
