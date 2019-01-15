6915a219b284dd5bd69fa449b92fd6f04481b803
Max <msuraev@sysmocom.de>
2019-01-15 16:51:40 +0100

    Prevent GCR encoder/decoder functions from being used directly
    They only make sense in the context of LCLS so far - let's make sure
    they're not used by external projects directly instead of gsm0808_*()
    counterparts.
    
    Change-Id: I4ae5a3472a20492d5f76170b722e4e2274a5c433

--- include/osmocom/gsm/gsm29205.h
+++ include/osmocom/gsm/gsm29205.h
@@ -37,6 +37,6 @@ struct osmo_gcr_parsed {
 	uint8_t cr[5];   /** Call Reference ID */
 };
 
-uint8_t osmo_enc_gcr(struct msgb *msg, const struct osmo_gcr_parsed *g);
-int osmo_dec_gcr(struct osmo_gcr_parsed *gcr, const uint8_t *elem, uint8_t len);
+uint8_t osmo_enc_gcr(struct msgb *msg, const struct osmo_gcr_parsed *g) OSMO_DEPRECATED_OUTSIDE_LIBOSMOCORE;
+int osmo_dec_gcr(struct osmo_gcr_parsed *gcr, const uint8_t *elem, uint8_t len) OSMO_DEPRECATED_OUTSIDE_LIBOSMOCORE;
 bool osmo_gcr_eq(const struct osmo_gcr_parsed *gcr1, const struct osmo_gcr_parsed *gcr2);
