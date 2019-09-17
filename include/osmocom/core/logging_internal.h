070628065aca80f89c9dceda9af63eef3b9849dc
Pau Espin Pedrol <pespin@sysmocom.de>
2019-09-17 18:23:05 +0200

    logging_internal.h: Fix osmo_log_info definition
    Global symbol osmo_log_info is declared in logging.c as non-const,
    because it is modified. As soon as logging_internal.h is included into
    logging.c, the compiler warns about osmo_log_info being declared twice
    differently.
    
    Change-Id: Iea961c3caeb12ddf60c99d4dca644bb9ab538767

--- include/osmocom/core/logging_internal.h
+++ include/osmocom/core/logging_internal.h
@@ -7,7 +7,7 @@
 #include <osmocom/core/utils.h>
 
 extern void *tall_log_ctx;
-extern const struct log_info *osmo_log_info;
+extern struct log_info *osmo_log_info;
 extern const struct value_string loglevel_strs[];
 
 void assert_loginfo(const char *src);
