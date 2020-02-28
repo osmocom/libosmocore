1c46d19e4316ffd898d505559b2af90e6b930e27
Philipp Maier <pmaier@sysmocom.de>
2020-02-28 14:23:55 +0100

    parity: add amr crc14 definition
    AMR not only specifies a 6 bit CRC for regular voice information. It also
    specifies a 14 bit CRC to protect the comfort noise updates contained in
    the SID_UPDATE frames.
    
    Change-Id: I5cfd8ca806aba8d42cb9787f69605cea7de6e900
    Related: OS#2978

--- include/osmocom/coding/gsm0503_parity.h
+++ include/osmocom/coding/gsm0503_parity.h
@@ -19,5 +19,6 @@ const struct osmo_crc16gen_code gsm0503_sch_crc10;
 const struct osmo_crc8gen_code gsm0503_tch_fr_crc3;
 const struct osmo_crc8gen_code gsm0503_tch_efr_crc8;
 const struct osmo_crc8gen_code gsm0503_amr_crc6;
+const struct osmo_crc16gen_code gsm0503_amr_crc14;
 
 /*! @} */
