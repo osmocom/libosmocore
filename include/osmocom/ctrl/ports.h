7dedebada5c5dc08e1c5aa4a90f5d1c41f948e91
Harald Welte <laforge@osmocom.org>
2020-03-08 09:38:37 +0100

    Add CTRL port number for osmo-mgw
    Related: OS#4441
    
    Change-Id: I70ae237ea27972f6819fd217c3d5059dda009486

--- include/osmocom/ctrl/ports.h
+++ include/osmocom/ctrl/ports.h
@@ -24,4 +24,6 @@
 #define OSMO_CTRL_PORT_GBPROXY	4263
 /* 4264 used by VTY interface */
 #define OSMO_CTRL_PORT_CBC	4265
+/* 4266 used by D-GSM mDNS */
+#define OSMO_CTRL_PORT_MGW	4267
 /* When adding/changing port numbers, keep docs and wiki in sync. See above. */
