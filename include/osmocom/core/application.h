f36b76211f8bae3f372f7202827a718e125706f1
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2020-10-23 21:05:07 +0700

    application: do not document unrelated forward-declarations
    Change-Id: Ic04caab15abbd0c0d3a01f6e128935a3ceed903e

--- include/osmocom/core/application.h
+++ include/osmocom/core/application.h
@@ -2,16 +2,13 @@
 
 #include <osmocom/core/defs.h>
 
+struct log_info;
+struct log_target;
+
 /*!
  * \file application.h
  * Routines for helping with the osmocom application setup.
  */
-
-/*! information containing the available logging subsystems */
-struct log_info;
-
-/*! one instance of a logging target (file, stderr, ...) */
-struct log_target;
 
 /*! the default logging target, logging to stderr */
 extern struct log_target *osmo_stderr_target;
