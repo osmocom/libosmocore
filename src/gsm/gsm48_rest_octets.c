osmocom/libosmocore

commit 946bb95af1838fcf026587c51a6ad36e34e202c3

GPRS Cell Options (SI13): Add REL-4 CCN_ACTIVE bit
This is required in order to tell MS that osmo-pcu now supports
Network Assisted Cell Change (NACC).

Related: SYS#4909
Change-Id: I2aaa8c1107c977f711c2d7530034f57e36e3a237

src/gsm/gsm48_rest_octets.c
@@ -906,6 +906,8 @@ static int append_gprs_cell_opt(struct bitvec *bv,
 		bitvec_set_bit(bv, 0);
 	} else {
 		/* extension information */
+
+		/* R99 extension: */
 		bitvec_set_bit(bv, 1);
 		if (!gco->ext_info.egprs_supported) {
 			/* 6bit length of extension */
@@ -927,6 +929,10 @@ static int append_gprs_cell_opt(struct bitvec *bv,
 		bitvec_set_bit(bv, gco->ext_info.pfc_supported);
 		bitvec_set_bit(bv, gco->ext_info.dtm_supported);
 		bitvec_set_bit(bv, gco->ext_info.bss_paging_coordination);
+
+		/* REL-4 extension: */
+		bitvec_set_bit(bv, gco->ext_info.ccn_active);
+		bitvec_set_bit(bv, 0); /* NW_EXT_UTBF disabled */
 	}
 
 	return 0;

TODO-RELEASE
@@ -8,5 +8,6 @@
 # If any interfaces have been removed or changed since the last public release: c:r:0.
 #library	what			description / commit summary line
 libosmogsm	new API			gsm0808_create_sapi_reject_cause() with cause argument
+libosmogsm	ABI change		struct osmo_gprs_cell_options size changed (new field at the end)
 libosmovty	ABI change		struct cmd_element: add a field for program specific attributes
 libosmovty	ABI change		struct vty_app_info: optional program specific attributes description

include/osmocom/gprs/protocol/gsm_04_60.h
@@ -167,7 +167,7 @@ enum osmo_gprs_nmo {
 	GPRS_NMO_III	= 2,	/* no paging coordination */
 };
 
-/* TS 04.60 12.24 */
+/* TS 44.060 12.24 */
 struct osmo_gprs_cell_options {
 	enum osmo_gprs_nmo nmo;
 	/* T3168: wait for packet uplink assignment message */
@@ -187,6 +187,7 @@ struct osmo_gprs_cell_options {
 		uint8_t pfc_supported;
 		uint8_t dtm_supported;
 		uint8_t bss_paging_coordination;
+		bool ccn_active;
 	} ext_info;
 };
 

commit 5b6f1d7d7a56f529d750197a38bcf8f5835f87b8

gsm: Fix wrong length in SI13 GPRS Cell Options IE
Older commit adding the 2 bits for Rel-4 extension forgot to increase
the length field (see TS 44.060 Table 12.24.1)

Fixes: 946bb95af1838fcf026587c51a6ad36e34e202c3
Change-Id: I20efb4403cdf6c5bc717502a7075630044142f17

src/gsm/gsm48_rest_octets.c
@@ -911,12 +911,12 @@ static int append_gprs_cell_opt(struct bitvec *bv,
 		bitvec_set_bit(bv, 1);
 		if (!gco->ext_info.egprs_supported) {
 			/* 6bit length of extension */
-			bitvec_set_uint(bv, (1 + 3)-1, 6);
+			bitvec_set_uint(bv, (1 + 5)-1, 6);
 			/* EGPRS supported in the cell */
 			bitvec_set_bit(bv, 0);
 		} else {
 			/* 6bit length of extension */
-			bitvec_set_uint(bv, (1 + 5 + 3)-1, 6);
+			bitvec_set_uint(bv, (1 + 5 + 5)-1, 6);
 			/* EGPRS supported in the cell */
 			bitvec_set_bit(bv, 1);
 