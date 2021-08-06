d8f8919a5850390dbbdd9dbea266f7f569abab7e
Oliver Smith <osmith@sysmocom.de>
2021-08-06 16:11:16 +0200

    vty/ports: prepare formatting for a long define
    Add one more tab between the define and the port number, to prepare for
    longer defines in the next patch.
    
    Related: OS#5203
    Change-Id: I46655e33651814f41a1fea93406a83334d2fc529

--- include/osmocom/vty/ports.h
+++ include/osmocom/vty/ports.h
@@ -9,32 +9,32 @@
 #pragma once
 
 /* 4236 used by control interface */
-#define OSMO_VTY_PORT_TRX	4237
+#define OSMO_VTY_PORT_TRX		4237
 /* 4238 used by osmo-bts control interface */
-#define OSMO_VTY_PORT_STP	4239
-#define OSMO_VTY_PORT_PCU	4240	/* also: osmo_pcap_client */
-#define OSMO_VTY_PORT_BTS	4241	/* also: osmo_pcap_server */
-#define OSMO_VTY_PORT_NITB_BSC	4242
-#define OSMO_VTY_PORT_BSC_MGCP	4243
-#define OSMO_VTY_PORT_MGW	OSMO_VTY_PORT_BSC_MGCP
-#define OSMO_VTY_PORT_BSC_NAT	4244
-#define OSMO_VTY_PORT_SGSN	4245
-#define OSMO_VTY_PORT_GBPROXY	4246
-#define OSMO_VTY_PORT_BB	4247
+#define OSMO_VTY_PORT_STP		4239
+#define OSMO_VTY_PORT_PCU		4240	/* also: osmo_pcap_client */
+#define OSMO_VTY_PORT_BTS		4241	/* also: osmo_pcap_server */
+#define OSMO_VTY_PORT_NITB_BSC		4242
+#define OSMO_VTY_PORT_BSC_MGCP		4243
+#define OSMO_VTY_PORT_MGW		OSMO_VTY_PORT_BSC_MGCP
+#define OSMO_VTY_PORT_BSC_NAT		4244
+#define OSMO_VTY_PORT_SGSN		4245
+#define OSMO_VTY_PORT_GBPROXY		4246
+#define OSMO_VTY_PORT_BB		4247
 /* 4249-4251 used by control interface */
-#define OSMO_VTY_PORT_BTSMGR	4252
-#define OSMO_VTY_PORT_GTPHUB	4253
-#define OSMO_VTY_PORT_MSC	4254
+#define OSMO_VTY_PORT_BTSMGR		4252
+#define OSMO_VTY_PORT_GTPHUB		4253
+#define OSMO_VTY_PORT_MSC		4254
 /* 4255 used by control interface */
-#define OSMO_VTY_PORT_MNCC_SIP	4256
+#define OSMO_VTY_PORT_MNCC_SIP		4256
 /* 4257 used by control interface */
-#define OSMO_VTY_PORT_HLR	4258
+#define OSMO_VTY_PORT_HLR		4258
 /* 4259 used by control interface */
-#define OSMO_VTY_PORT_GGSN	4260
-#define OSMO_VTY_PORT_HNBGW	4261
+#define OSMO_VTY_PORT_GGSN		4260
+#define OSMO_VTY_PORT_HNBGW		4261
 /* 4262-4263 used by control interface */
-#define OSMO_VTY_PORT_CBC	4264
-#define OSMO_VTY_PORT_UECUPS	4268
-#define OSMO_VTY_PORT_E1D	4269
-#define OSMO_VTY_PORT_SMLC	4271
+#define OSMO_VTY_PORT_CBC		4264
+#define OSMO_VTY_PORT_UECUPS		4268
+#define OSMO_VTY_PORT_E1D		4269
+#define OSMO_VTY_PORT_SMLC		4271
 /* When adding/changing port numbers, keep docs and wiki in sync. See above. */
