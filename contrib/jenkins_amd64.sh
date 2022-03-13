381dc6109950e28492c68834502c85f43f4f2bcc
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2022-03-13 17:32:29 +0300

    contrib/jenkins_amd64.sh: remove FreeBSD specific quirks
    We don't run build verification on FreeBSD anymore.
    
    Change-Id: Ia3a9a49cade453a937ee230163ca44930a39d639

--- contrib/jenkins_amd64.sh
+++ contrib/jenkins_amd64.sh
@@ -3,19 +3,13 @@
 
 . $(dirname "$0")/jenkins_common.sh
 
-ENABLE_SANITIZE="--enable-sanitize"
-
-if [ "x$label" = "xFreeBSD_amd64" ]; then
-        ENABLE_SANITIZE=""
-fi
-
 src_dir="$PWD"
 build() {
     build_dir="$1"
 
     prep_build "$src_dir" "$build_dir"
 
-    "$src_dir"/configure  --disable-silent-rules --enable-static $ENABLE_SANITIZE --enable-werror \
+    "$src_dir"/configure  --disable-silent-rules --enable-static --enable-sanitize --enable-werror \
         --enable-external-tests
 
     run_make
