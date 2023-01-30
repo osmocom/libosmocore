osmocom/libosmocore

commit 600d4eeab71665c0aec0a11a226d231f109534ca

SI: add RR short PD message types
Related: OS#5783
Change-Id: Ifbd0aabe826298fa4715eb4eb2ff8363e765933d

src/gsm/gsm48.c
@@ -419,6 +419,29 @@ const char *gsm48_rr_msg_name(uint8_t msgtype)
 	return get_value_string(rr_msg_names, msgtype);
 }
 
+/* 3GPP TS 44.018 Table 10.4.2 */
+static const struct value_string rr_msg_type_short_names[] = {
+	{ GSM48_MT_RR_SH_SI10,		"System Information Type 10" },
+	{ GSM48_MT_RR_SH_FACCH,		"Notification/FACCH" },
+	{ GSM48_MT_RR_SH_UL_FREE,	"Uplink Free" },
+	{ GSM48_MT_RR_SH_MEAS_REP,	"Enhanced Measurement Report (uplink)" },
+	{ GSM48_MT_RR_SH_MEAS_INFO,	"Measurement Information (downlink)" },
+	{ GSM48_MT_RR_SH_VGCS_RECON,	"VBS/VGCS Reconfigure" },
+	{ GSM48_MT_RR_SH_VGCS_RECON2,	"VBS/VGCS Reconfigure2" },
+	{ GSM48_MT_RR_SH_VGCS_INFO,	"VGCS Additional Information" },
+	{ GSM48_MT_RR_SH_VGCS_SMS,	"VGCS SMS Information" },
+	{ GSM48_MT_RR_SH_SI10bis,	"System Information Type 10bis" },
+	{ GSM48_MT_RR_SH_SI10ter,	"System Information Type 10ter" },
+	{ GSM48_MT_RR_SH_VGCS_NEIGH,	"VGCS Neighbour Cell Information" },
+	{ GSM48_MT_RR_SH_APP_DATA,	"Notify Application Data" },
+	{ 0,				NULL }
+};
+
+/*! return string representation of RR Message Type using the RR short protocol discriminator */
+const char *gsm48_rr_msg_type_short_name(uint8_t msgtype)
+{
+	return get_value_string(rr_msg_type_short_names, msgtype);
+}
 
 const struct value_string gsm48_chan_mode_names[] = {
 	{ GSM48_CMODE_SIGN,		"SIGNALLING" },

include/osmocom/gsm/protocol/gsm_04_08.h
@@ -1540,6 +1540,21 @@ void gsm48_set_dtx(struct gsm48_cell_options *op, enum gsm48_dtx_mode full,
 
 #define GSM48_MT_RR_APP_INFO		0x38
 
+/* 3GPP TS 44.018 Table 10.4.2 */
+#define GSM48_MT_RR_SH_SI10		0x0
+#define GSM48_MT_RR_SH_FACCH		0x1
+#define GSM48_MT_RR_SH_UL_FREE		0x2
+#define GSM48_MT_RR_SH_MEAS_REP		0x4
+#define GSM48_MT_RR_SH_MEAS_INFO	0x5
+#define GSM48_MT_RR_SH_VGCS_RECON	0x6
+#define GSM48_MT_RR_SH_VGCS_RECON2	0x7
+#define GSM48_MT_RR_SH_VGCS_INFO	0x8
+#define GSM48_MT_RR_SH_VGCS_SMS		0x9
+#define GSM48_MT_RR_SH_SI10bis		0xA
+#define GSM48_MT_RR_SH_SI10ter		0xB
+#define GSM48_MT_RR_SH_VGCS_NEIGH	0xC
+#define GSM48_MT_RR_SH_APP_DATA		0xD
+
 /* Table 10.2/3GPP TS 04.08 */
 #define GSM48_MT_MM_IMSI_DETACH_IND	0x01
 #define GSM48_MT_MM_LOC_UPD_ACCEPT	0x02

commit bc12728679f8d42e9e8b2ff7639effdf0a4fa767

SI: add missing header
Previous SI10 patch added function without exposing it via public header.
Let's fix this.

Fixes: 600d4eeab71665c0aec0a11a226d231f109534ca
Change-Id: Ia7530e9c8a21f6f99f3aac7baea5cbb38763c4f3

include/osmocom/gsm/gsm48.h
@@ -35,6 +35,7 @@ extern const struct tlv_definition gsm48_mm_att_tlvdef;
 const char *gsm48_cc_state_name(uint8_t state);
 const char *gsm48_cc_msg_name(uint8_t msgtype);
 const char *gsm48_rr_msg_name(uint8_t msgtype);
+const char *gsm48_rr_short_pd_msg_name(uint8_t msgtype);
 const char *rr_cause_name(uint8_t cause);
 const char *osmo_rai_name(const struct gprs_ra_id *rai);
 char *osmo_rai_name_buf(char *buf, size_t buf_len, const struct gprs_ra_id *rai);

src/gsm/gsm48.c
@@ -438,7 +438,7 @@ static const struct value_string rr_msg_type_short_names[] = {
 };
 
 /*! return string representation of RR Message Type using the RR short protocol discriminator */
-const char *gsm48_rr_msg_type_short_name(uint8_t msgtype)
+const char *gsm48_rr_short_pd_msg_name(uint8_t msgtype)
 {
 	return get_value_string(rr_msg_type_short_names, msgtype);
 }

src/gsm/libosmogsm.map
@@ -330,6 +330,7 @@ osmo_gsm48_rest_octets_si13_decode;
 osmo_gsm48_rest_octets_si13_encode;
 osmo_gsm48_rest_octets_si3_decode;
 osmo_gsm48_rest_octets_si4_decode;
+gsm48_rr_short_pd_msg_name;
 gsm48_rr_msg_name;
 gsm48_cc_state_name;
 gsm48_construct_ra;