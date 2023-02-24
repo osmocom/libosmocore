osmocom/libosmocore

commit 812dfbf3a756088ef8ae083044c690783ef84067

ASCI: add VBS/VGCS support to BTS features list
Related: OS#5783
Change-Id: I583a170070c192c6c47fae41ce923a7439561b4f

src/gsm/bts_features.c
@@ -44,6 +44,8 @@ const struct value_string osmo_bts_features_descs[] = {
 	{ BTS_FEAT_DYN_TS_SDCCH8,	"Dynamic Timeslot configuration as SDCCH8" },
 	{ BTS_FEAT_ACCH_TEMP_OVP,	"FACCH/SACCH Temporary overpower" },
 	{ BTS_FEAT_OSMUX,		"Osmux (Osmocom RTP multiplexing)" },
+	{ BTS_FEAT_VBS,			"Voice Broadcast Service" },
+	{ BTS_FEAT_VGCS,		"Voice Group Call Service" },
 	{ 0, NULL }
 };
 

include/osmocom/gsm/bts_features.h
@@ -34,6 +34,8 @@ enum osmo_bts_features {
 	BTS_FEAT_DYN_TS_SDCCH8, /* Osmo Dynamic TS supports configured as SDCCH8 */
 	BTS_FEAT_ACCH_TEMP_OVP, /* FACCH/SACCH Temporary overpower */
 	BTS_FEAT_OSMUX, /* Osmux (Osmocom RTP muxing) support */
+	BTS_FEAT_VBS, /* Voice Broadcast Service support, 3GPP TS 43.069 */
+	BTS_FEAT_VGCS, /* Voice Group Call Service support, 3GPP TS 44.068 */
 	_NUM_BTS_FEAT
 };
 

commit f4f5d54ea2cb47a51aeaec2b5fb990664755900a

gsm: add missing features to osmo_bts_features_names[]
Change-Id: I0ff47a79d611cd1c2d23ac5b4d53dca27e402821
Fixes: 812dfbf3a756088ef8ae083044c690783ef84067

src/gsm/bts_features.c
@@ -82,5 +82,7 @@ const struct value_string osmo_bts_features_names[] = {
 	{ BTS_FEAT_DYN_TS_SDCCH8, "DYN_TS_SDCCH8" },
 	{ BTS_FEAT_ACCH_TEMP_OVP, "ACCH_TEMP_OVP" },
 	{ BTS_FEAT_OSMUX, "OSMUX" },
+	{ BTS_FEAT_VBS, "VBS" },
+	{ BTS_FEAT_VGCS, "VGCS" },
 	{}
 };