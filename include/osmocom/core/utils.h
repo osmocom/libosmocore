osmocom/libosmocore

commit 0f59cebf081edc20d8a7b19cb799810446fc2ab3

utils: improve readability of OSMO_STRBUF_CHAR_COUNT
Similarly to OSMO_STRBUF_REMAIN, let's improve the code readability
by adding a static inline function.  We should generally prefer using
static inline functions over macros, unless there is something that
only the proprocessor can do.

Change-Id: I71f24b87c13fd83952029171a6993f8da5e32e5b

include/osmocom/core/utils.h
@@ -297,11 +297,21 @@ static inline size_t _osmo_strbuf_remain(const struct osmo_strbuf *sb)
 #define OSMO_STRBUF_REMAIN(STRBUF) \
 	_osmo_strbuf_remain(&(STRBUF))
 
+/*! Get number of actual characters (without terminating nul) in the given struct osmo_strbuf.
+ * \param[in] sb  the string buffer to get the number of characters for.
+ * \returns number of actual characters (without terminating nul). */
+static inline size_t _osmo_strbuf_char_count(const struct osmo_strbuf *sb)
+{
+	if (OSMO_UNLIKELY(sb == NULL || sb->buf == NULL))
+		return 0;
+	if (sb->pos == NULL || sb->pos <= sb->buf)
+		return 0;
+	return OSMO_MIN(sb->pos - sb->buf, sb->len - 1);
+}
+
 /*! Return number of actual characters contained in struct osmo_strbuf (without terminating nul). */
-#define OSMO_STRBUF_CHAR_COUNT(STRBUF) ((STRBUF).buf && ((STRBUF).pos > (STRBUF).buf) ? \
-					OSMO_MIN((STRBUF).pos - (STRBUF).buf, \
-						 (STRBUF).len - 1) \
-					: 0)
+#define OSMO_STRBUF_CHAR_COUNT(STRBUF) \
+	_osmo_strbuf_char_count(&(STRBUF))
 
 /*! Like OSMO_STRBUF_APPEND(), but for function signatures that return the char* buffer instead of a length.
  * When using this function, the final STRBUF.chars_needed may not reflect the actual number of characters needed, since

commit fb4ce0b029cb52278b8d835c71ce54743226c239

utils: fix -Wsign-compare in definition of OSMO_STRBUF_CHAR_COUNT
We're seeing tons of -Wsign-compare warnings since I merged 0f59cebf:

include/osmocom/core/utils.h: In function 'size_t _osmo_strbuf_char_count(const osmo_strbuf*)':
include/osmocom/core/utils.h:24:29: error: comparison of integer expressions of different
                                    signedness: 'long int' and 'long unsigned int'
                                    [-Werror=sign-compare]
   24 | #define OSMO_MIN(a, b) ((a) >= (b) ? (b) : (a))
      |                         ~~~~^~~~~~
include/osmocom/core/utils.h:309:16: note: in expansion of macro 'OSMO_MIN'
  309 |         return OSMO_MIN(sb->pos - sb->buf, sb->len - 1);
      |                ^~~~~~~~

Interestingly enough, this -Wsign-compare problem has always been the
case, even before commit 0f59cebf.  And somehow this did not show up
when building libosmocore.git, but only when building C++ projects
(osmo-pcu and osmo-trx).

Perhaps it has something to do with how g++ compiles extern "C" code.

Change-Id: I8e396459409e4260b8715f9e890e8972d4609a31
Fixes: 0f59cebf "utils: improve readability of OSMO_STRBUF_CHAR_COUNT"

include/osmocom/core/utils.h
@@ -306,7 +306,7 @@ static inline size_t _osmo_strbuf_char_count(const struct osmo_strbuf *sb)
 		return 0;
 	if (sb->pos == NULL || sb->pos <= sb->buf)
 		return 0;
-	return OSMO_MIN(sb->pos - sb->buf, sb->len - 1);
+	return OSMO_MIN((size_t)(sb->pos - sb->buf), sb->len - 1);
 }
 
 /*! Return number of actual characters contained in struct osmo_strbuf (without terminating nul). */