d8ac06847819fb35c2d15b68a4ae2bcec20c4bfe
Mychaela N. Falconia <falcon@freecalypso.org>
2024-05-22 00:40:22 +0000

    include/osmocom/gsm: add rtp_extensions.h
    Previous patches add definitions of GSM0808_IE_THEMWI_RTP_EXTENSIONS
    and RSL_IE_OSMO_RTP_EXTENSIONS to BSSMAP and RSL; this new header file
    provides definitions for individual bits in the single value octet
    of the new IE.
    
    Related: OS#6448
    Change-Id: I0eccfe5ddcf44f8f20440acb01e2d4870ec0cd91

---
+++ include/osmocom/gsm/rtp_extensions.h
@@ -0,0 +1,23 @@
+/*
+ * Themyscira Wireless Technical Specification TW-TS-003 defines a BSSMAP
+ * extension whereby a CN implementation and a BSS implementation can
+ * negotiate the use of non-3GPP-standard extensions to RTP user plane,
+ * extensions that modify RTP formats counter to the stipulations of
+ * 3GPP TS 48.103.  There is also a private Osmocom-defined IE in Abis RSL
+ * that communicates the same RTP extensions from OsmoBSC to OsmoBTS.
+ *
+ * This header file defines the meaning of the bits in the first (and currently
+ * only) value octet of the TLV IE added to BSSMAP and RSL interfaces,
+ * namely, GSM0808_IE_THEMWI_RTP_EXTENSIONS and RSL_IE_OSMO_RTP_EXTENSIONS.
+ * It is based on this authoritative definition:
+ *
+ * https://www.freecalypso.org/specs/tw-ts-003-v010002.txt
+ *
+ * Section 5.3 in the above specification defines the assignment of
+ * individual bits in the single value octet.
+ */
+
+#pragma once
+
+#define	OSMO_RTP_EXT_TWTS001	0x01
+#define	OSMO_RTP_EXT_TWTS002	0x02
