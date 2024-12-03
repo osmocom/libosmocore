89c40ae1b81787049fbd40a36e6e05dc44846f26
Alexander Couzens <lynxis@fe80.eu>
2024-12-03 12:43:55 +0100

    gprs_ns2_sns: add include osmocom/core/talloc.h
    Talloc is directly used, do an explicit include talloc
    and not depend on a another header to include it.
    
    Change-Id: I867c5e3f87efcf5bcc99713a0c1dcaecfc55fcbd

--- src/gb/gprs_ns2_sns.c
+++ src/gb/gprs_ns2_sns.c
@@ -47,6 +47,7 @@
 #include <osmocom/core/msgb.h>
 #include <osmocom/core/socket.h>
 #include <osmocom/core/sockaddr_str.h>
+#include <osmocom/core/talloc.h>
 #include <osmocom/gsm/tlv.h>
 #include <osmocom/gprs/gprs_msgb.h>
 #include <osmocom/gprs/gprs_ns2.h>
