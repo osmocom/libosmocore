osmocom/libosmocore

commit df1ee8568b97dbf6d5268a83d1715a1c1fffb2de

osmo_io: Track IOFD_FLAG_FD_REGISTERED in all backends
This will be used in common segmentation handling code to figure out
whether the iofd is still registered or was unregistered by the user
during the read cb, in order to know whether to continue submitting read
events upwards or to discard the handling.

Change-Id: Id5e92aa22ce1c5d76028c539784118be227b9d5a

src/core/osmo_io.c
@@ -737,12 +737,20 @@ int osmo_iofd_register(struct osmo_io_fd *iofd, int fd)
 {
 	int rc = 0;
 
-	if (fd >= 0)
-		iofd->fd = fd;
-	else if (iofd->fd < 0) {
+	if (fd < 0 && iofd->fd < 0) {
 		/* this might happen if both osmo_iofd_setup() and osmo_iofd_register() are called with -1 */
 		LOGPIO(iofd, LOGL_ERROR, "Cannot register io_fd using invalid fd == %d\n", iofd->fd);
 		return -EBADF;
+	}
+	if (fd < 0)
+		fd = iofd->fd;
+	else if (iofd->fd < 0)
+		iofd->fd = fd;
+
+	if (IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED)) {
+		/* If re-registering same fd, handle as NO-OP.
+		 * New FD should go through unregister() first. */
+		return iofd->fd == fd ? 0 : -ENOTSUP;
 	}
 
 	rc = osmo_iofd_ops.register_fd(iofd);
@@ -750,6 +758,8 @@ int osmo_iofd_register(struct osmo_io_fd *iofd, int fd)
 		return rc;
 
 	IOFD_FLAG_UNSET(iofd, IOFD_FLAG_CLOSED);
+	IOFD_FLAG_SET(iofd, IOFD_FLAG_FD_REGISTERED);
+
 	if ((iofd->mode == OSMO_IO_FD_MODE_READ_WRITE && iofd->io_ops.read_cb) ||
 	    (iofd->mode == OSMO_IO_FD_MODE_RECVFROM_SENDTO && iofd->io_ops.recvfrom_cb) ||
 	    (iofd->mode == OSMO_IO_FD_MODE_RECVMSG_SENDMSG && iofd->io_ops.recvmsg_cb)) {
@@ -772,7 +782,14 @@ int osmo_iofd_register(struct osmo_io_fd *iofd, int fd)
  */
 int osmo_iofd_unregister(struct osmo_io_fd *iofd)
 {
-	return osmo_iofd_ops.unregister_fd(iofd);
+	int rc;
+
+	if (!IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED))
+		return 0;
+
+	rc = osmo_iofd_ops.unregister_fd(iofd);
+	IOFD_FLAG_UNSET(iofd, IOFD_FLAG_FD_REGISTERED);
+	return rc;
 }
 
 /*! Retrieve the number of messages pending in the transmit queue.

src/core/osmo_io_poll.c
@@ -128,29 +128,18 @@ static int iofd_poll_register(struct osmo_io_fd *iofd)
 	struct osmo_fd *ofd = &iofd->u.poll.ofd;
 	int rc;
 
-	if (IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED))
-		return 0;
-
 	osmo_fd_setup(ofd, iofd->fd, 0, &iofd_poll_ofd_cb_dispatch, iofd, 0);
 	if (IOFD_FLAG_ISSET(iofd, IOFD_FLAG_NOTIFY_CONNECTED))
 		osmo_fd_write_enable(&iofd->u.poll.ofd);
 
 	rc = osmo_fd_register(ofd);
-	if (!rc)
-		IOFD_FLAG_SET(iofd, IOFD_FLAG_FD_REGISTERED);
-
 	return rc;
 }
 
 static int iofd_poll_unregister(struct osmo_io_fd *iofd)
 {
 	struct osmo_fd *ofd = &iofd->u.poll.ofd;
-
-	if (!IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED))
-		return 0;
 	osmo_fd_unregister(ofd);
-	IOFD_FLAG_UNSET(iofd, IOFD_FLAG_FD_REGISTERED);
-
 	return 0;
 }
 

commit 8972da953da57126c6eff2cc23662d61cb37b712

osmo_iofd_register: fix the case of changing fd
Doxygen description for this function states:

  \param[in] fd the system fd number that will be registered.
  If you did not yet specify the file descriptor number during
  osmo_fd_setup(), or if it has changed since then, you can state
  the [new] file descriptor number as argument.
  If you wish to proceed with the previously specified file descriptor
  number, pass -1.

However, the case where a new fd is passed to osmo_iofd_register()
while the structure contains an old (but unregistered) fd was not
handled correctly: the code would proceed with re-registering the old
fd, ignoring the newly passed one.

Fixes: df1ee8568b97dbf6d5268a83d1715a1c1fffb2de
Change-Id: If8b8486ad7934afa203dfe1e996c9217373a017b

src/core/osmo_io.c
@@ -746,20 +746,22 @@ int osmo_iofd_register(struct osmo_io_fd *iofd, int fd)
 {
 	int rc = 0;
 
-	if (fd < 0 && iofd->fd < 0) {
+	if (IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED)) {
+		/* If re-registering same fd, handle as NO-OP.
+		 * And it is an even more explicit NO-OP
+		 * if the caller passed in -1. */
+		if (fd < 0 || fd == iofd->fd)
+			return 0;
+		/* New FD should go through unregister() first. */
+		return -ENOTSUP;
+	}
+
+	if (fd >= 0)
+		iofd->fd = fd;
+	if (iofd->fd < 0) {
 		/* this might happen if both osmo_iofd_setup() and osmo_iofd_register() are called with -1 */
 		LOGPIO(iofd, LOGL_ERROR, "Cannot register io_fd using invalid fd == %d\n", iofd->fd);
 		return -EBADF;
-	}
-	if (fd < 0)
-		fd = iofd->fd;
-	else if (iofd->fd < 0)
-		iofd->fd = fd;
-
-	if (IOFD_FLAG_ISSET(iofd, IOFD_FLAG_FD_REGISTERED)) {
-		/* If re-registering same fd, handle as NO-OP.
-		 * New FD should go through unregister() first. */
-		return iofd->fd == fd ? 0 : -ENOTSUP;
 	}
 
 	rc = osmo_iofd_ops.register_fd(iofd);