9396e27cc6645396852cc61a86277ab112e8da33
Mychaela N. Falconia <falcon@freecalypso.org>
2025-10-08 17:20:30 +0000

    gsm/protocol/gsm_48_103.h: new header file
    3GPP TS 48.103 prescribes a set of fixed RTP payload type numbers
    for use on GSM AoIP interface - however, until now there was no
    Osmocom header file that formally captures these spec definitions.
    Fix this omission.
    
    Change-Id: Id41d4dffd0eb562df4fc948b237d629e858e495b

---
+++ include/osmocom/gsm/protocol/gsm_48_103.h
@@ -0,0 +1,21 @@
+/*
+ * This header file captures the set of fixed RTP payload type definitions
+ * specified in 3GPP TS 48.103 (GSM AoIP interface) Table 5.4.2.2.1.
+ */
+
+#pragma once
+
+/* uncompressed speech */
+#define	OSMO_AOIP_RTP_PT_PCMU		0
+#define	OSMO_AOIP_RTP_PT_PCMA		8
+
+/* compressed speech */
+#define	OSMO_AOIP_RTP_PT_FR1		3
+#define	OSMO_AOIP_RTP_PT_EFR		110
+#define	OSMO_AOIP_RTP_PT_HR1		111
+#define	OSMO_AOIP_RTP_PT_AMR		112
+#define	OSMO_AOIP_RTP_PT_AMRWB		113
+
+/* circuit-switched data */
+#define	OSMO_AOIP_RTP_PT_CSD		120	/* without redundancy */
+#define	OSMO_AOIP_RTP_PT_CSD_RED	121	/* with    redundancy */
