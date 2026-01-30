osmocom/libosmocore

commit bc400626b28c85fe0f52ce25947a90650b0d06b5

logging: Avoid infinite loop logging inside logging path
If eg. gsmtap_log target is requested to log a message but the iofd
wqueue is full, it may try to log an error message, which will create an
infinite stack loop.

Change-Id: If3b8c0ee97537242162558fdd1c99f03b32af811

src/core/logging.c
@@ -84,7 +84,7 @@ static struct log_context log_context;
 void *tall_log_ctx = NULL;
 LLIST_HEAD(osmo_log_target_list);
 
-static __thread long int logging_tid;
+__thread struct log_thread_state log_thread_state;
 
 #if (!EMBEDDED)
 /*! One global copy that contains the union of log levels for all targets
@@ -606,9 +606,9 @@ int log_output_buf(char *buf, int buf_len, struct log_target *target, unsigned i
 			OSMO_STRBUF_PRINTF(sb, " ");
 		}
 		if (target->print_tid) {
-			if (logging_tid == 0)
-				logging_tid = (long int)osmo_gettid();
-			OSMO_STRBUF_PRINTF(sb, "%ld ", logging_tid);
+			if (log_thread_state.tid == 0)
+				log_thread_state.tid = (long int)osmo_gettid();
+			OSMO_STRBUF_PRINTF(sb, "%ld ", log_thread_state.tid);
 		}
 		if (target->print_category)
 			OSMO_STRBUF_PRINTF(sb, "%s%s%s%s ",
@@ -743,6 +743,14 @@ void osmo_vlogp(int subsys, int level, const char *file, int line,
 {
 	struct log_target *tar;
 
+	if (OSMO_UNLIKELY(log_thread_state.logging_active)) {
+		/* Avoid re-entrant logging: If logging to log target generates
+		 * extra logging (eg. an error log line due to some wqueue being full),
+		 * we may end up in an infinite loop. */
+		return;
+	}
+	log_thread_state.logging_active = true;
+
 	subsys = map_subsys(subsys);
 
 #if !defined(EMBEDDED)
@@ -776,6 +784,7 @@ void osmo_vlogp(int subsys, int level, const char *file, int line,
 	}
 
 	log_tgt_mutex_unlock();
+	log_thread_state.logging_active = false;
 }
 
 /*! logging function used by DEBUGP() macro

include/osmocom/core/logging_internal.h
@@ -4,16 +4,25 @@
  *  @{
  * \file logging_internal.h */
 
+#include <stdbool.h>
 #include <osmocom/core/logging.h>
 #include <osmocom/core/utils.h>
 
 /* maximum length of the log string of a single log event (typically  line) */
 #define MAX_LOG_SIZE   4096
 
+struct log_thread_state {
+	/* Whether we are inside a code path to generate logging output: */
+	bool logging_active;
+	/* Cache TID: */
+	long int tid;
+};
+
 extern void *tall_log_ctx;
 extern struct log_info *osmo_log_info;
 extern const struct value_string loglevel_strs[];
 extern struct llist_head osmo_log_target_list;
+extern __thread struct log_thread_state log_thread_state;
 
 void assert_loginfo(const char *src);
 

commit e9cb0eaddf677fd7e279578185468930bd0ee08e

Fix 'logging: Avoid infinite loop logging inside logging path'
I didn't see the early return of log_cache_check(), which should leave
the logging_active variable as true forever.

Fixes: bc400626b28c85fe0f52ce25947a90650b0d06b5
Change-Id: I13ed182688597c7da279a50bf057eff6d13f0867

src/core/logging.c
@@ -743,14 +743,6 @@ void osmo_vlogp(int subsys, int level, const char *file, int line,
 {
 	struct log_target *tar;
 
-	if (OSMO_UNLIKELY(log_thread_state.logging_active)) {
-		/* Avoid re-entrant logging: If logging to log target generates
-		 * extra logging (eg. an error log line due to some wqueue being full),
-		 * we may end up in an infinite loop. */
-		return;
-	}
-	log_thread_state.logging_active = true;
-
 	subsys = map_subsys(subsys);
 
 #if !defined(EMBEDDED)
@@ -758,6 +750,13 @@ void osmo_vlogp(int subsys, int level, const char *file, int line,
 		return;
 #endif
 
+	if (OSMO_UNLIKELY(log_thread_state.logging_active)) {
+		/* Avoid re-entrant logging: If logging to log target generates
+		 * extra logging (eg. an error log line due to some wqueue being full),
+		 * we may end up in an infinite loop. */
+		return;
+	}
+	log_thread_state.logging_active = true;
 	log_tgt_mutex_lock();
 
 	llist_for_each_entry(tar, &osmo_log_target_list, entry) {