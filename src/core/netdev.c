8137e66198f1fec077f0ed3beb0b418010bf5345
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2026-03-16 05:37:35 +0700

    netdev: netdev_mnl_set_mtu(): drop unneeded variables
    Change-Id: I8f9d39733a80a0ec4e8c462f4ae32b4ac52b5849

--- src/core/netdev.c
+++ src/core/netdev.c
@@ -541,8 +541,6 @@ static int netdev_mnl_set_mtu(struct osmo_mnl *omnl, unsigned int if_index,
 	char buf[MNL_SOCKET_BUFFER_SIZE];
 	struct nlmsghdr *nlh = mnl_nlmsg_put_header(buf);
 	struct ifinfomsg *ifm;
-	unsigned int change = 0;
-	unsigned int flags = 0;
 
 	nlh->nlmsg_type	= RTM_NEWLINK;
 	nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK;
@@ -550,8 +548,8 @@ static int netdev_mnl_set_mtu(struct osmo_mnl *omnl, unsigned int if_index,
 
 	ifm = mnl_nlmsg_put_extra_header(nlh, sizeof(*ifm));
 	ifm->ifi_family = AF_UNSPEC;
-	ifm->ifi_change = change;
-	ifm->ifi_flags = flags;
+	ifm->ifi_change = 0;
+	ifm->ifi_flags = 0;
 	ifm->ifi_index = if_index;
 
 	mnl_attr_put_u32(nlh, IFLA_MTU, mtu);
